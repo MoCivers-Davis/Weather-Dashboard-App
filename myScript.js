3168582c31f6ae221a7bb0f8390a8aec9bdbd0f1
Mo Civ <mocivers@comcast.net>
2020-02-19 22:43:49 -0500

    uploaded my final code

--- myScript.js
+++ myScript.js
@@ -1,13 +1,51 @@
-var APIKey = "6adc5c999e3d81602171dd96dbf6ecca";
-var cityName =   $(".dreamCity").val();
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + APIKey
-var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey
-console.log (queryURL2);
-console.log (cityName);
-$.ajax({
-    url: queryURL2,
-    method: "GET"
-  })
-  .then(function(response) {
-console.log (response);
-  })
+
+// ==========CLick Function================
+$(".submitBt").on("click", function () {
+    var cityName = $(".dreamCity").val();
+    getWeather(cityName);
+    getFiveDay(cityName);
+    console.log(cityName);
+    $("ol").append(cityName);
+})
+// ==========Open Weather API================
+let APIKey = "6adc5c999e3d81602171dd96dbf6ecca";
+
+function getWeather(cityName) {
+    let queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey
+    console.log(queryURL2);
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+            $(".cityName").empty();
+
+            // ==========location where current weather data is displayed================     
+            $(".currentCity").html(" Current City:</h1>" + "<h1>" + response.name);
+            $(".wind-speed").text("Wind Speed: " + response.wind.speed);
+            $(".humidity").text("Humidity: " + response.main.humidity);
+            $(".temperature").text("Temperature (F): " + response.main.temp);
+            $(".uv-index").text("UV Index: " + response.main.temp);
+
+            let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            $(".tempF").text("Temperature (Kelvin) " + tempF);
+
+            console.log("Wind Speed: " + response.wind.speed);
+            console.log("Humidity: " + response.main.humidity);
+            console.log("Temperature (F): " + response.main.temp);
+
+        })
+}
+function getFiveDay(cityName) {
+    let queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey
+    console.log(queryURL3);
+    $.ajax({
+        url: queryURL3,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+
+}
